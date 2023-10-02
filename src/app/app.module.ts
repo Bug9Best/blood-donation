ae62a1dee2c2bf727b1012e324f3948f0e914abd
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-02 17:47:27 +0700

    import and declared module that use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { PrimeNGModule } from './modules/primeng.module';
+import { AppLayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PrimeNGModule,
+    AppLayoutModule,
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    ConfirmationService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
