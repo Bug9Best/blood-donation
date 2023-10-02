dc41c83aac2ddf1ef32c39bd33e209f48a379f03
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-02 17:40:22 +0700

    setup menu

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppTopBarComponent } from './topbar/topbar.component';
+import { AppFooterComponent } from './footer/footer.component';
+import { AppMenuComponent } from './menu/menu.component';
+import { AppMenuitemComponent } from './munuItem/menuitem.component';
+import { AppSidebarComponent } from './sidebar/sidebar.component';
+import { AppLayoutComponent } from './layout.component';
+import { AppConfigModule } from './config/config.module';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { PrimeNGModule } from '../modules/primeng.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppTopBarComponent,
+    AppFooterComponent,
+    AppMenuComponent,
+    AppMenuitemComponent,
+    AppSidebarComponent,
+    AppLayoutComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    // HttpClientModule,
+    // BrowserAnimationsModule,
+    // InputTextModule,
+    // SidebarModule,
+    // BadgeModule,
+    // RadioButtonModule,
+    // InputSwitchModule,
+    // RippleModule,
+    PrimeNGModule,
+    RouterModule,
+    AppConfigModule
+  ]
+})
+export class AppLayoutModule { }
