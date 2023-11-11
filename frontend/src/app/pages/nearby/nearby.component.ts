f779bf2609e1ac931b2c1c6ba98d96086cc0e601
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-11-12 03:54:08 +0700

    partial done nearby

--- frontend/src/app/pages/nearby/nearby.component.ts
+++ frontend/src/app/pages/nearby/nearby.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class NearbyComponent {
 
+  listArea: any = [
+    1, 2, 3, 4, 5, 6
+  ]
+
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+    disableDoubleClickZoom: true,
+    mapTypeControl: false,
+    center: { lat: 13.723784979498301, lng: 100.74317612937757 },
+
+    maxZoom: 15,
+    minZoom: 0,
+  };
+
+  constructor() { }
+
+  onSearch(keyWord: string) {
+    console.log(keyWord);
+  }
+
 }
--- frontend/src/app/pages/nearby/nearby.module.ts
+++ frontend/src/app/pages/nearby/nearby.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrimeNGModule } from 'src/app/modules/primeng.module';
 import { ThaiModule } from 'src/app/modules/thai.module';
 import { NearbyComponent } from './nearby.component';
+import { PageTitleComponent } from 'src/app/component/page-title/page-title.component';
+import { SearchbarComponent } from 'src/app/component/searchbar/searchbar.component';
+import { GoogleMapsModule, } from '@angular/google-maps';
+
 
 const routes: Routes = [
   {
@@ -13,12 +17,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NearbyComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     PrimeNGModule,
-    ThaiModule
+    ThaiModule,
+    PageTitleComponent,
+    SearchbarComponent,
+    GoogleMapsModule
   ]
 })
 export class NearbyModule { }
