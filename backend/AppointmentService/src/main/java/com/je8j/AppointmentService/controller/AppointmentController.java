c0c22203f039b8ea949c0597c0657cd1be3a43f8
7E8J <103146285+7E8J@users.noreply.github.com>
2023-11-12 18:37:58 +0700

    AppointmentService

--- backend/AppointmentService/src/main/java/com/je8j/AppointmentService/controller/AppointmentController.java
+++ backend/AppointmentService/src/main/java/com/je8j/AppointmentService/controller/AppointmentController.java
@@ -2,8 +2,6 @@ package com.je8j.AppointmentService.controller;
 
 import com.je8j.AppointmentService.pojo.Appointment;
 import com.je8j.AppointmentService.repository.AppointmentService;
-import com.rabbitmq.client.impl.Environment;
-import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +9,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppointmentController {
-    private final Environment env;
-    private final CommandGateway commandGateway;
-
     private final AppointmentService appointmentService;
     private final RabbitTemplate rabbitTemplate;
 
     @Autowired
-    public AppointmentController(AppointmentService appointmentService, RabbitTemplate rabbitTemplate, Environment env, CommandGateway commandGateway) {
+    public AppointmentController(AppointmentService appointmentService, RabbitTemplate rabbitTemplate) {
         this.appointmentService = appointmentService;
         this.rabbitTemplate = rabbitTemplate;
-        this.env = env;
-        this.commandGateway = commandGateway;
     }
 
     @RequestMapping(value ="/appointment", method = RequestMethod.GET)
