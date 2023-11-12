835fd1f04cfb457b1ab744053eadb20eee27068c
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-11-12 09:04:12 +0700

    partial done of appointment

--- frontend/src/app/pages/appointment/appointment.component.ts
+++ frontend/src/app/pages/appointment/appointment.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+export enum StepAppoint {
+  Defalut = 0,
+  Question = 1,
+  Appointment = 2,
+}
 
 @Component({
   selector: 'app-appointment',
@@ -6,5 +13,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment.component.scss']
 })
 export class AppointmentComponent {
+  curretstep: StepAppoint = StepAppoint.Defalut;
+  currentQuestion: number = 0;
 
+  constructor(
+    private messageService: MessageService,
+  ) { }
+
+  showMessages(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      severity: severity,
+      summary: summary,
+      detail: detail
+    });
+  }
+
+  cancelQuestion() {
+    this.currentQuestion = 0;
+    this.curretstep = StepAppoint.Defalut;
+  }
+
+  toQuestion() {
+    this.curretstep = StepAppoint.Question;
+  }
+
+  toAppointment() {
+    this.currentQuestion = 0;
+    this.curretstep = StepAppoint.Appointment;
+  }
+
+  showQuestion(value: number) {
+    this.currentQuestion = value;
+  }
+
+  createAppointment(value: any) {
+    this.showMessages('success', 'Agendamento', 'Agendamento realizado com sucesso!');
+  }
 }
