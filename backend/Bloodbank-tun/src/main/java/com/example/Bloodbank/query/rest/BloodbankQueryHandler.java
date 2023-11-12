75cfc62e80e7eca5b00fb3a9e6db9c6da20e52a6
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-11-13 02:00:37 +0700

    update

---
+++ backend/Bloodbank-tun/src/main/java/com/example/Bloodbank/query/rest/BloodbankQueryHandler.java
@@ -0,0 +1,32 @@
+package com.example.Bloodbank.query.rest;
+
+import com.example.Bloodbank.core.data.BloodbankEntity;
+import com.example.Bloodbank.core.data.BloodbankRepository;
+import com.example.Bloodbank.query.FindBloodbankQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BloodbankQueryHandler {
+    private final BloodbankRepository bloodbankRepository;
+
+    public BloodbankQueryHandler(BloodbankRepository bloodbankRepository){
+        this.bloodbankRepository = bloodbankRepository;
+    }
+
+    @QueryHandler
+    public List<BloodbankRestModel> findBloodbank(FindBloodbankQuery query){
+        List<BloodbankRestModel> bloodbankRest = new ArrayList<>();
+        List<BloodbankEntity> storedBloodbank = bloodbankRepository.findAll();
+        for (BloodbankEntity bloodbankEntity : storedBloodbank) {
+            BloodbankRestModel bloodbankRestModel = new BloodbankRestModel();
+            BeanUtils.copyProperties(bloodbankEntity, bloodbankRestModel);
+            bloodbankRest.add(bloodbankRestModel);
+        }
+        return bloodbankRest;
+    }
+}
