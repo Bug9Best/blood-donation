59b07f0935c4058bb481a4a763d8bbe90074b107
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-11-12 22:40:07 +0700

    update

--- backend/AuthService/src/main/java/com/example/AuthService/repository/UserService.java
+++ backend/AuthService/src/main/java/com/example/AuthService/repository/UserService.java
@@ -15,19 +15,6 @@ public class UserService {
         this.repository = repository;
     }
 
-//    @Cacheable(value="userList")
-    public List<User> retrieveUsers(){
-        return repository.findAll();
-    }
-
-    public User retrieveUserByName(String firstname) {
-        return repository.findByName(firstname);
-    }
-//    @Cacheable(value="user", key="#id")
-    public User retrieveUserByID(String _id) {
-        return repository.findByID(_id);
-    }
-
     public User validate(String email, String password) {
         User user = repository.findByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
@@ -41,21 +28,6 @@ public class UserService {
         return repository.save(user);
     }
 
-//    @CachePut(value="user", key="#wizard.getName()")
-//    @CacheEvict(value="userList", allEntries=true)
-    public User updateUser(User user) {
-        return repository.save(user);
-    }
 
-//    @CacheEvict(value="wizard", key="#wizard.getName()")
-//    @Caching(evict={ @CacheEvict(value="wizardList", allEntries=true) })
-    public boolean deleteUser(User user) {
-        try {
-            repository.delete(user);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
 }
