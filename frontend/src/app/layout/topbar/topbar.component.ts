eef5c5eea5e5775cc1602d11029638137fbfbf95
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-11-12 09:16:09 +0700

    change style of topbar

--- frontend/src/app/layout/topbar/topbar.component.ts
+++ frontend/src/app/layout/topbar/topbar.component.ts
@@ -1,20 +1,43 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "../service/layout.service";
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './topbar.component.html',
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
 })
 export class AppTopBarComponent {
 
-    items!: MenuItem[];
+  items: MenuItem[] = [
+    {
+      label: 'ตัวเลือก',
+      items: [
+        {
+          label: 'ประวัติส่วนตัว',
+          icon: 'pi pi-user',
+          command: () => {
+            this.router.navigate(['/profile']);
+          }
+        },
+        {
+          label: 'ออกจากระบบ',
+          icon: 'pi pi-sign-out',
+          command: () => {
+          }
+        }
+      ]
+    },
+  ];
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
+  @ViewChild('menubutton') menuButton!: ElementRef;
 
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
-    @ViewChild('topbarmenu') menu!: ElementRef;
+  @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+  constructor(
+    public layoutService: LayoutService,
+    private router: Router
+  ) { }
 }
