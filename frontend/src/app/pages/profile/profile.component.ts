af2e7ba1ffe00141788da0c2575ad378c4b4f578
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-11-12 18:27:27 +0700

    update

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
   editMode: boolean = false;
+
+  formData: FormGroup = new FormGroup({
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phonenumber: new FormControl(null, Validators.required),
+    bloodgroup: new FormControl(null, Validators.required),
+    gender: new FormControl(null, Validators.required),
+    dateofbirth: new FormControl(null, Validators.required),
+    weight: new FormControl(null, Validators.required),
+    height: new FormControl(null, Validators.required),
+    congenitaldisease: new FormControl(null, Validators.required),
+    userimage: new FormGroup(null, Validators.required),
+  });
 
   editProfile() {
     this.editMode = true;
